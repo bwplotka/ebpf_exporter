37f105628223899e450cb1a09dd89d322d9de7a5
Bartlomiej Plotka <bwplotka@gmail.com>
2021-09-15 11:38:55 +0200

    Further speed up; added singleton.
    Signed-off-by: Bartlomiej Plotka <bwplotka@gmail.com>

--- decoder/cgroup.go
+++ decoder/cgroup.go
@@ -32,20 +32,29 @@ func (c *CGroup) Decode(in []byte, _ config.Decoder) ([]byte, error) {
 		return []byte(path), nil
 	}
 
+	fmt.Println("not found ", string(in))
+
 	if err := c.refreshCache(); err != nil {
 		log.Printf("Error refreshing cgroup id to path map: %s", err)
+		if path, ok := c.cache[uint64(cgroupID)]; ok {
+			// Cache unknown result on successful refresh.
+			return []byte(path), nil
+		}
+		return []byte(fmt.Sprintf("unknown_cgroup_id:%d", cgroupID)), nil
 	}
 
-	if path, ok := c.cache[uint64(cgroupID)]; ok {
-		return []byte(path), nil
+	if _, ok := c.cache[uint64(cgroupID)]; !ok {
+		// Cache unknown result on successful refresh.
+		c.cache[uint64(cgroupID)] = fmt.Sprintf("unknown_cgroup_id:%d", cgroupID)
 	}
 
-	return []byte(fmt.Sprintf("unknown_cgroup_id:%d", cgroupID)), nil
+	return []byte(c.cache[uint64(cgroupID)]), nil
 }
 
 func (c *CGroup) refreshCache() error {
 	byteOrder := bcc.GetHostByteOrder()
 
+	newCache := make(map[uint64]string, len(c.cache)) // Reset cache.
 	if err := filepath.Walk("/sys/fs/cgroup", func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
@@ -60,13 +69,16 @@ func (c *CGroup) refreshCache() error {
 			log.Printf("Error resolving handle of %s: %s", path, err)
 			return nil
 		}
-
-		c.cache[byteOrder.Uint64(handle.Bytes())] = path
+		newCache[byteOrder.Uint64(handle.Bytes())] = path
 		return nil
 	}); err != nil {
+		// Update cache on error cases, to have at least partial results.
+		for k, v := range newCache {
+			c.cache[k] = v
+		}
 		return err
 	}
-
+	c.cache = newCache
 	log.Println("refreshed cgroup cache, keys: ", len(c.cache))
 	return nil
 }
